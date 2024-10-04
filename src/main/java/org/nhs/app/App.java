ce0a79b1030ba0d378c29959990e7cd8913dc33b
nishthapr <2000195@apps.nsd.org>
2024-10-04 19:00:16 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,67 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
+   public static void main (String[]args){
+   mystery2();
+   mystery3();
+    mystery4();
+    challenge();
+   }
 
-package org.nhs.app;
+   public static void mystery2(){
+    for(int i= 1; i<6;i++){
+        for(int j = 1;j<=i;j++){
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+   }
 
-// Your code goes here
+   public static void mystery3(){
+    for(int i= 1; i<6;i++){
+        for (int k = 4;k>=(i);k--){
+            System.out.print(".");
+        }
+        for(int j = 1;j<=i;j++){
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+   }
+
+   public static void mystery4(){
+    for(int i= 1; i<6;i++){
+        for (int k = 4;k>=(i);k--){
+            System.out.print(".");
+        }
+        System.out.print(i);
+        for(int j = 1;j<(i);j++){
+            System.out.print(".");
+        }
+        System.out.println();
+    }
+   }
+
+   public static void challenge(){
+
+    for(int i= 1; i<10;i++){
+        for(int j= (9-i); j>0;j--){
+            System.out.print(" ");
+        }
+       
+        for(int k= 1; k<=(2*i);k++){
+            System.out.print(i);
+        }
+       System.out.println();
+   }
+    for(int i= 8; i>0;i--){
+        for(int j= (9-i); j>0;j--){
+            System.out.print(" ");
+        }
+       
+        for(int k= 1; k<=(2*i);k++){
+            System.out.print(i);
+        }
+       System.out.println();
+   }
+}
+
+}
